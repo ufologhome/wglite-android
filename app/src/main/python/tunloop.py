18958c9b3f743cd4518ad0bf2d32c8a94c5cb093
ufologhome <38780109+ufologhome@users.noreply.github.com>
2025-12-26 18:03:18 +0300

    Create tunloop.py

---
+++ app/src/main/python/tunloop.py
@@ -0,0 +1,13 @@
+import os
+import socket
+
+udp_sock = None
+
+def tun_loop(tun_fd, server_ip, server_port):
+    global udp_sock
+    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        data = os.read(tun_fd, 2048)
+        if not data:
+            continue
+        udp_sock.sendto(data, (server_ip, server_port))
