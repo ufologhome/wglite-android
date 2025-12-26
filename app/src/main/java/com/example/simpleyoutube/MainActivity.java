0001fb3c3bac0193aeb655b65c8e7404ac4179cd
ufologhome <38780109+ufologhome@users.noreply.github.com>
2025-12-26 19:15:01 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/example/simpleyoutube/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.simpleyoutube;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btnYoutube);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://www.youtube.com")
+                );
+                startActivity(intent);
+            }
+        });
+    }
+}
