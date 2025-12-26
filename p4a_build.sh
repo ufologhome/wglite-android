f974a7c5c80b809d9ec0971f8b4ada7fb56225a6
ufologhome <38780109+ufologhome@users.noreply.github.com>
2025-12-26 10:11:37 +0300

    Create p4a_build.sh

---
+++ p4a_build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+export ANDROIDSDK=$HOME/android-sdk
+export ANDROIDNDK=$HOME/android-ndk/android-ndk-r21e
+export PATH=$ANDROIDSDK/cmdline-tools/latest/bin:$PATH
+
+APP_DIR=$(pwd)/app
+
+python3 -m pythonforandroid.toolchain create \
+  --dist_name=wglite \
+  --bootstrap=service_only \
+  --requirements=python3,pyjnius \
+  --arch=armeabi-v7a \
+  --copy-libs \
+  --color=always \
+  --ndk-api=25 \
+  --ignore-setup-py \
+  --debug
+
+python3 -m pythonforandroid.toolchain apk \
+  --name WG-Lite \
+  --package org.example.wglite \
+  --version 0.1 \
+  --private $APP_DIR \
+  --bootstrap service_only \
+  --arch armeabi-v7a \
+  --sdk 29 \
+  --minsdk 25 \
+  --manifest $APP_DIR/android_manifest.tmpl.xml \
+  --permission INTERNET \
+  --debug
